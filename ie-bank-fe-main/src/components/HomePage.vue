1247820435c07d5c1a85c9e486371b4f1ed5a345
riyadmazari <rmazari.ieu2021@student.ie.edu>
2023-10-16 00:19:38 +0200

    adding the Home Page

---
+++ ie-bank-fe-main/src/components/HomePage.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+      <router-link to="/skull">
+        <button type="button" class="btn btn-primary">Test Connection</button>
+      </router-link>
+    
+      <router-link to="/accounts">
+        <button type="button" class="btn btn-primary">View Accounts</button>
+      </router-link>
+    </div>
+  </template>
